933aa63ba8566e3a117d510e82e1cf28b9bc4b8a
Bruno Henrique Nunes <bnunes@bi4all.pt>
2024-07-18 21:43:36 -0300

    adjusted

--- tasks.py
+++ tasks.py
@@ -163,8 +163,8 @@ class APNewsScraper:
             logging.info("No pop-up found.")
 
         try:
-            self.driver.wait_until_element_is_visible("xpath://div[@id='onetrust-close-btn-container']/button", timeout=1)
-            self.driver.click_element("xpath://div[@id='onetrust-close-btn-container']/button")
+            self.driver.wait_until_element_is_visible("xpath://*[@id='onetrust-accept-btn-handler']", timeout=1)
+            self.driver.click_element("xpath://*[@id='onetrust-accept-btn-handler']")
             time.sleep(1)
             logging.info("Button closed.")
         except:
